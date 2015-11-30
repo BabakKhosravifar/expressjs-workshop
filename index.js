e9875f1756ae03c305d6f92bd94bdf6bfb1d239f
babakkhosravifar <babak.khosravifar@gmail.com>
2015-11-30 20:24:39 +0000

    exercise4

--- index.js
+++ index.js
@@ -1,8 +1,53 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+var entries = {
+  1: {
+    firstName: "Babak",
+    lastName: "Khosravifar",
+    emails: [
+      {type: "home", address: "Babak.Khosravifar@gmail.com"},
+      {type: "work", address: "Babak.Khosravifar@Mentorina.com"}
+    ]
+  },
+  2: {
+    firstName: "Ali",
+    lastName: "Hatamni",
+    emails: [
+      {type: "home", address: "Ali.Hatamni@gmail.com"},
+      {type: "work", address: "Ali.Hatamni@mcgill.ca"}
+    ]
+  },
+  3: {
+    firstName: "Fred",
+    lastName: "Smith",
+    emails: [
+      {type: "home", address: "Fred.Smith@gmail.com"},
+      {type: "work", address: "Fred.Smith@live.ca"}
+    ]
+  },
+  4: {
+    firstName: "Ali",
+    lastName: "Ashoori",
+    emails: [
+      {type: "home", address: "Ali.Ashoori@gmail.com"},
+      {type: "work", address: "Ali.Ashoori@bell.ca"}
+    ]
+  }  
+};
+
+
+
+
+app.get('/entry/:entryId', function (req, res) {
+  
+  var entryId = req.params.entryId;
+  
+  if(entryId<5){
+    res.json(entries[entryId]);
+  } else {
+    res.status(400).send('Bad request: The entryId is not available.')
+  }
 });
 
 
