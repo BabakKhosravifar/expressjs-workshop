94df854c5f5d83886072ecff594b9cc14e4281f6
babakkhosravifar <babak.khosravifar@gmail.com>
2015-11-30 19:25:50 +0000

    exercise3

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+
+app.get('/op/:operation/:number1/:number2', function (req, res) {
+  
+  var operation = req.params.operation;
+  var number1 = Number(req.params.number1);
+  var number2 = Number(req.params.number2);
+  var solution;
+  var op;
+  switch(operation){
+    
+    case "add":
+      solution = number1+number2;
+      op = "+";
+      break;
+    case "sub":
+      solution = number1-number2;
+      op = "-";
+      break;
+    case "mult":
+      solution = number1*number2;
+      op = "*";
+      break;
+    case "div":
+      
+      if(number2!==0) solution = number1/number2;
+      else solution = null;
+      op = "/";
+      break;
+    default:
+        res.status(400).send("Bad request: the operator not found among add/sub/mult/div. ");
+        break;
+  }
+  
+  var calculation = { 
+    text: "Here is your requested calculation: "+number1 +' '+ op + ' '+number2+ ' = '+solution+'.'
+      
+  };
+  
+  return res.json(calculation.text);
 });
-
-
 
 
 
