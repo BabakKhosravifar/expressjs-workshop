ab1951aae987fa72295d079ef13a4b826fa01e61
babakkhosravifar <babak.khosravifar@gmail.com>
2015-11-30 21:04:48 +0000

    exercise5

--- index.js
+++ index.js
@@ -1,11 +1,65 @@
 var express = require('express');
 var app = express();
+var url = require('url');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+
+
+var entries = {
+  1: {
+    firstName: "Babak",
+    lastName: "Khosravifar",
+    emails: [
+      {type: "home", address: "Babak.Khosravifar@gmail.com"},
+      {type: "work", address: "Babak.Khosravifar@Mentorina.com"}
+    ]
+  },
+  2: {
+    firstName: "Ali",
+    lastName: "Hatamni",
+    emails: [
+      {type: "home", address: "Ali.Hatamni@gmail.com"},
+      {type: "work", address: "Ali.Hatamni@mcgill.ca"}
+    ]
+  },
+  3: {
+    firstName: "Fred",
+    lastName: "Smith",
+    emails: [
+      {type: "home", address: "Fred.Smith@gmail.com"},
+      {type: "work", address: "Fred.Smith@live.ca"}
+    ]
+  },
+  4: {
+    firstName: "Ali",
+    lastName: "Ashoori",
+    emails: [
+      {type: "home", address: "Ali.Ashoori@gmail.com"},
+      {type: "work", address: "Ali.Ashoori@bell.ca"}
+    ]
+  }  
+};
+
+
+
+app.get('/entry/search', function (req, res) {
+
+  var firstName = req.query.firstName.toUpperCase();
+  var lastName = req.query.lastName.toUpperCase();
+  var keysInEntries = Object.keys(entries);
+  var resultsFound = [];
+  var found = false;
+
+  for(var i = 1 ; i <= keysInEntries.length ; i++){
+    if(firstName === entries[i].firstName.toUpperCase() || lastName === entries[i].lastName.toUpperCase())
+      resultsFound.push(entries[i]);
+  }
+  
+  if(!resultsFound){
+    res.send("Error: No entry found.");
+  } else {
+    res.json(resultsFound);
+  }
 });
-
-
 
 
 
